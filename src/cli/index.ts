78f4e9ccfa87f12580c67e03ef6221dea33fd1ca
Daniel Esquivias <daniel.esquivias@gmail.com>
2020-03-09 10:31:37 -0700

    Removed Helper Functions

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
+
+const fs = require("fs");
 import { existsSync, readFileSync } from "fs";
 import { CookieJar } from "tough-cookie";
 import Auth from "../auth";
 import Asset from "../asset";
-import Workflow from "../workflow";
+import Workflow, {
+	Data as WorkflowData,
+	StepCommandsData as WorkflowStepCommandsData
+} from "../workflow";
 import { ResultCodeType } from "../model";
-import Helper from "../helper";
 
 try {
-	const helper = new Helper();
 	const path = "./crownpeak.config.json";
 	if (existsSync(path)) {
 		const crownpeakConfig = JSON.parse(readFileSync(path, "utf8"));
@@ -31,11 +34,11 @@ try {
 				asset
 					.fields({ assetId: crownpeakConfig.test.assetId })
 					.then(response => {
-						if (helper.assetHasFieldByName(response, "body")) {
-							console.log(
-								helper.assetGetFieldByName(response, "body")
-									.value
-							);
+						const field = response.fields.find(
+							field => field.name == "body"
+						);
+						if (field) {
+							console.log(field.value);
 							asset
 								.update({
 									assetId: crownpeakConfig.test.assetId,
@@ -48,58 +51,108 @@ try {
 									console.log(response.resultCode);
 									const workflow = new Workflow(config);
 									workflow.read().then(response => {
-										const wd = helper.workflowGetByName(
-											response,
-											crownpeakConfig.test.workflow.name
-										);
-										const wc = helper.workflowGetCommandByName(
-											wd,
-											crownpeakConfig.test.workflow
-												.command
-										);
-										asset
-											.executeWorkflowCommand({
-												assetId:
-													crownpeakConfig.test
-														.assetId,
-												commandId: wc.commandId,
-												skipDependencies: false
-											})
-											.then(response => {
+										var wd, wc;
+										for (const v of Object.values(
+											response.workflows
+										)) {
+											const data = (v as unknown) as WorkflowData;
+											if (
+												data.name ==
+												crownpeakConfig.test.workflow
+													.name
+											) {
+												wd = data;
+												for (const v of Object.values(
+													wd.stepCommands
+												)) {
+													const data = (v as unknown) as WorkflowStepCommandsData;
+													data.commands.forEach(
+														commandStep => {
+															if (
+																commandStep.command ==
+																crownpeakConfig
+																	.test
+																	.workflow
+																	.command
+															) {
+																wc = commandStep;
+																return;
+															}
+														}
+													);
+												}
+												break;
+											}
+										}
+										if (wd && wc) {
+											asset
+												.executeWorkflowCommand({
+													assetId:
+														crownpeakConfig.test
+															.assetId,
+													commandId: wc.commandId,
+													skipDependencies: false
+												})
+												.then(response => {
+													console.log(
+														response.resultCode
+													);
+												});
+											// upload test
+											const fileBytes = fs
+												.readFileSync(
+													`./${crownpeakConfig.test.fileName}`
+												)
+												.toString("base64");
+											asset
+												.upload({
+													bytes: fileBytes,
+													destinationFolderId:
+														crownpeakConfig.test
+															.destinationFolderId,
+													workflowId: wd.id,
+													newName:
+														crownpeakConfig.test
+															.fileName,
+													modelId:
+														crownpeakConfig.test
+															.modelId
+												})
+												.then(response => {
+													asset
+														.attach({
+															assetId:
+																response.asset
+																	.id,
+															bytes: fileBytes,
+															originalFilename:
+																crownpeakConfig
+																	.test
+																	.fileName
+														})
+														.then(response => {
+															console.log(
+																"attach:"
+															);
+															console.log(
+																response
+															);
+														});
+													console.log("upload:");
+													console.log(response);
+												});
+										} else {
+											if (wd == null) {
 												console.log(
-													response.resultCode
+													`Unknown Workflow Name: ${crownpeakConfig.test.workflow.name}`
 												);
-											});
-
-										// upload test
-										asset
-											.upload({
-												bytes: helper.encodeFileToBase64(
-													"test.png"
-												),
-												destinationFolderId: 545252,
-												workflowId: wd.id,
-												newName: "test.png",
-												modelId: 801
-											})
-											.then(response => {
-												asset
-													.attach({
-														assetId:
-															response.asset.id,
-														bytes: helper.encodeFileToBase64(
-															"test.png"
-														),
-														originalFilename:
-															"google.png"
-													})
-													.then(response => {
-														console.log("attach:");
-														console.log(response);
-													});
-												console.log("upload:");
-												console.log(response);
-											});
+											}
+											if (wc == null) {
+												console.log(
+													`Unknown Workflow Command: ${crownpeakConfig.test.workflow.command}`
+												);
+											}
+										}
 									});
 								});
 						}
